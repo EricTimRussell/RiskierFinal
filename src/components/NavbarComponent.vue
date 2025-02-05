e8c93561d62b8fbf06d0072c38b14a0371cf6bcb
Eric Russell <Erictim.Russell@live.com>
2025-02-04 21:10:28 -0700

    navbar layout

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-olive container-fluid shadow-lg">
+  <div class="bg-green container-fluid shadow-lg">
     <div class="row">
       <div class="col-3 d-flex">
         <RouterLink to="/">
           <img class="logo img-fluid" src="../assets/logo/Riskier-logo.png">
         </RouterLink>
       </div>
-      <nav class="gap-3 col-9 d-flex justify-content-center" v-motion-slide-top>
+      <nav class="gap-3 col-9 d-flex justify-content-evenly" v-motion-slide-top>
         <RouterLink to="dashboard">
           <span title="Dashboard" aria-current="page" class="nav-link active material-symbols-outlined ">
             sports_esports
@@ -28,6 +28,39 @@
           </span>
         </RouterLink>
       </nav>
+      <div class="col-12 d-flex justify-content-evenly pt-4 pb-2 px-2 text-light">
+        <div v-motion-roll-bottom>
+          <span class="material-symbols-outlined text-warning fs-1">
+            attach_money
+          </span>
+          <span title="Total Capital" class="fs-1 text">0</span>
+          <!-- <span title="Total Capital" class="fs-1 text-danger">0</span> -->
+        </div>
+        <div v-motion-roll-bottom>
+          <span class="material-symbols-outlined text-secondary fs-1">
+            factory
+          </span>
+          <span title="Total Industry" class="fs-1">0</span>
+          <!-- <span title="Total Industry" v-if="teams.totalIndustry < 0" class="fs-1 text-danger">0</span> -->
+        </div>
+        <div v-motion-roll-bottom>
+          <span class="material-symbols-outlined text-success fs-1">
+            psychiatry
+          </span>
+          <span title="Total Agriculture" class="fs-1">0</span>
+          <!-- <span title="Total Agriculture" v-if="teams.totalAgriculture < 0" class="fs-1 text-danger">{{
+            teams.totalAgriculture }}</span> -->
+        </div>
+        <div v-motion-roll-bottom>
+          <span class="material-symbols-outlined text-dark fs-1">
+            build
+          </span>
+          <span title="Total Production" class="fs-1">0</span>
+          <!-- <span title="Total Production" v-if="teams.totalProduction < 0" class="fs-1 text-danger">{{
+            teams.totalProduction
+            }}</span> -->
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,11 +71,6 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <style scoped>
-.bg-olive{
-  background-color: #7c8a71;
-  padding: 10px;
-}
-
 .router-link-active {
   color: #FFDA96;
   font-size: 40px;
@@ -69,7 +97,7 @@ a:active {
 /* Mobile */
 @media only screen and (max-width: 1023px) {
   .logo{
-    max-width: 100px;
+    max-width: 90px;
   }
 }
 
